99ff77dad030c32b5274dbf24c9a4fa815bd7ee9
Eetu <eetumaki01@gmail.com>
2024-04-03 13:20:43 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 //App.js
 import React from 'react'
+import { Header } from './components/Header'
+import { Score } from './components/Score'
+import { AddScore } from './components/AddScore'
+
 import './App.css'
 
 function App() {
   return(
     <div>
-      Hello World!
+      <Header />
+      <div className='container'>
+        <Score />
+        <AddScore />
+      </div>
     </div>
   );
 }
---
+++ src/components/Score.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Score = () => {
+  return (
+    <div>
+        <div className='score-container'>
+            <h4>Player1 Score:</h4>
+            <h1 id='score1'>301</h1> 
+        </div>
+        <div className='score-container'>
+            <h4>Player2 Score:</h4>
+            <h1 id='score2'>301</h1> 
+        </div>
+    </div>
+  )
+}
